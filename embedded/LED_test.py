e2eacc784043d1e76ec545d2ef0a365e7e333efe
Matthew Noto <notomatthew31@gmail.com>
2025-02-15 23:02:40 -0800

    working LEDs

---
+++ embedded/LED_test.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+import time
+import random
+import board
+import adafruit_dotstar as dotstar
+# On-board DotStar for boards including Gemma, Trinket, and ItsyBitsy
+# dots = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
+# Using a DotStar Digital LED Strip with 72 LEDs connected to hardware SPI
+dots = dotstar.DotStar(board.SCK, board.MOSI, 19, brightness=0.2)
+# Using a DotStar Digital LED Strip with 30 LEDs connected to digital pins
+# dots = dotstar.DotStar(board.D6, board.D5, 30, brightness=0.2)
+# HELPERS
+# a random color 0 -> 192
+def random_color():
+    return random.randrange(0, 7) * 32
+# MAIN LOOP
+n_dots = len(dots)
+try:
+    while True:
+        # Fill each dot with a random color
+        for dot in range(n_dots):
+            dots[dot] = (random_color(), random_color(), random_color())
+        time.sleep(0.25)
+except KeyboardInterrupt:
+    # Turn off all LEDs when interrupted
+    for dot in range(n_dots):
+        dots[dot] = (0, 0, 0)
+    print("Keyboard Interrupt caught. LEDs turned off.")
